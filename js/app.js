7fe521c88159da53bf8c91b8ee7e7fc195e38713
edimar-m <edimar.morles@gmail.com>
2021-07-19 22:10:16 -0300

    ADD/Añadiendo y hasheando la API key

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-
-/*********************************  CLAVES KEYS *******************************/
-//Hashear nuestras keys (encriptar)
-const privada = '745b5e34dc8dbfd68ba1bb8d9ee5511b38b356aa';
-const publica = '519eb1362038e44871da07e7549b508b';
-const timestamp = Date.now();
-
-const hash = md5(timestamp + privada + publica);
-console.log(hash)
+//API Key
+//Hashear nuestras keys
+const private = '745b5e34dc8dbfd68ba1bb8d9ee5511b38b356aa';
+const public = '519eb1362038e44871da07e7549b508b';
+const timestamp = Date.now(); // miliseegundos de donde se hace la llamada
+const hash = md5(timestamp + private + public);
+const totalData = document.getElementById("total-result");
